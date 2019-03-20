84218ee4a38d0bde6a0ea431fc9b2696be8262dd
Billy Andersson <anderssonbilly@hotmail.com>
2019-03-20 18:43:08 +0100

    TfIdf class created for holding data

--- SimpleSearchEngine/src/main/java/se/anderssonbilly/simplesearchengine/tfidf/TfIdf.java
+++ SimpleSearchEngine/src/main/java/se/anderssonbilly/simplesearchengine/tfidf/TfIdf.java
@@ -1,5 +1,59 @@
 package se.anderssonbilly.simplesearchengine.tfidf;
 
 public class TfIdf {
+	private double tf;
+	private double idf;
+	private double tfIdf;
+	
+	private double termCount;
+	private double docTermsCount;
+	private double docsCount;
+	
+	public TfIdf(double docTermsCount, double docsCount) {
+		this.setTermCount(0);
+		this.setDocTermsCount(docTermsCount);
+		this.setDocsCount(docsCount);
+	}
+	
+	public void calcTfIdf(double docsTermCount) {
+		this.tf = termCount/docTermsCount;
+		this.idf = Math.log(docsCount/docsTermCount);
+		this.tfIdf = this.tf * this.idf;
+	}
+	
+	public double getTf() {
+		return this.tf;
+	}
+	
+	public double getIdf() {
+		return this.idf;
+	}
+	
+	public double getTfIdf() {
+		return this.tfIdf;
+	}
+	
+	public double getTermCount() {
+		return termCount;
+	}
 
+	public void setTermCount(double termCount) {
+		this.termCount = termCount;
+	}
+
+	public double getDocTermsCount() {
+		return docTermsCount;
+	}
+
+	public void setDocTermsCount(double docTermsCount) {
+		this.docTermsCount = docTermsCount;
+	}
+
+	public double getDocsCount() {
+		return docsCount;
+	}
+
+	public void setDocsCount(double docsCount) {
+		this.docsCount = docsCount;
+	}
 }
