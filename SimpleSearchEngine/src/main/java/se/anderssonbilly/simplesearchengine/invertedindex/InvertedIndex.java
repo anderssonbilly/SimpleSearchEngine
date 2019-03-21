a3db95594e88cc58f916e64473a2b3a8c91aa048
Billy Andersson <anderssonbilly@hotmail.com>
2019-03-21 16:42:36 +0100

    Implemented index creation and sorting after TfIdf value

--- SimpleSearchEngine/src/main/java/se/anderssonbilly/simplesearchengine/invertedindex/InvertedIndex.java
+++ SimpleSearchEngine/src/main/java/se/anderssonbilly/simplesearchengine/invertedindex/InvertedIndex.java
@@ -1,7 +1,10 @@
 package se.anderssonbilly.simplesearchengine.invertedindex;
 
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 import se.anderssonbilly.simplesearchengine.documentloader.DocumentLoader;
@@ -36,13 +39,43 @@ public class InvertedIndex implements IInvertedIndex{
 	private Map<String,Map<String,TfIdf>> createIndex(Map<String,String> docsMap){
 		this.invertedIndexMap = new HashMap<>();
 		
-		
+		docsMap.forEach((docName, content) -> {
+			String[] terms = content.split(SPLIT_REGEX);
+			for(String term: terms) {
+				term = term.trim().toLowerCase();
+				if(term.length()>0) {
+					Map<String,TfIdf> docTfIdfMap = invertedIndexMap.get(term);
+					if(docTfIdfMap == null) {
+						docTfIdfMap = new HashMap<>();
+						invertedIndexMap.put(term, docTfIdfMap);
+					}
+					TfIdf tfIdf = docTfIdfMap.get(docName);
+					if(tfIdf == null)
+						tfIdf = new TfIdf(terms.length, docTfIdfMap.size());
+					tfIdf.setTermCount(tfIdf.getTermCount() +1);
+					docTfIdfMap.put(docName, tfIdf);
+				}
+			}
+		});
 		
 		return calcTfIdfAndSort(invertedIndexMap);
 	}
 	
 	private Map<String,Map<String,TfIdf>> calcTfIdfAndSort(Map<String, Map<String,TfIdf>> unSortedIndex){
 		Map<String,Map<String,TfIdf>> sortedIndex = new TreeMap<>();
+		
+		Comparator<Map.Entry<String,TfIdf>> byTfIdf = (Entry<String,TfIdf> e1, Entry<String,TfIdf> e2) -> {
+			return Double.compare(e2.getValue().getTfIdf(), e1.getValue().getTfIdf());
+		};
+		
+		unSortedIndex.forEach((term,docs)->{
+			docs.entrySet().forEach(doc->{
+				doc.getValue().calcTfIdf(docs.size());
+			});
+			LinkedHashMap<String,TfIdf> sortedDocs = new LinkedHashMap<>();
+			docs.entrySet().stream().sorted(byTfIdf).forEach(entry -> sortedDocs.put(entry.getKey(), entry.getValue()));
+			sortedIndex.put(term, sortedDocs);
+		});
 		
 		return sortedIndex;
 	}
