dd904f30fe3490cf28cf7753cb3f07ef3094b78b
Billy Andersson <anderssonbilly@hotmail.com>
2019-03-25 20:34:14 +0100

    Added JUnit tests

--- SimpleSearchEngine/src/test/java/se/anderssonbilly/simplesearchengine/testFileLoader.java
+++ SimpleSearchEngine/src/test/java/se/anderssonbilly/simplesearchengine/testFileLoader.java
@@ -1,10 +1,19 @@
 package se.anderssonbilly.simplesearchengine;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import se.anderssonbilly.simplesearchengine.documentloader.DocumentLoader;
 import se.anderssonbilly.simplesearchengine.documentloader.FileLoader;
 import se.anderssonbilly.simplesearchengine.invertedindex.InvertedIndex;
 import se.anderssonbilly.simplesearchengine.tfidf.TfIdf;
@@ -23,4 +32,109 @@ class testFileLoader {
 		index = new InvertedIndex(loader);
 	}
 	
+
+	@Test
+	void testReturnValue() {
+		expected.put("Document 1.txt", new TfIdf(0, 0));
+		expected.put("Document 2.txt", new TfIdf(0, 0));
+
+		Set<String> test = index.getInvertedIndex().get("brown").keySet();
+
+		assertTrue(test.containsAll(expected.keySet()) && !test.contains("Document 3.txt"));
+	}
+
+	@Test
+	void testFilter() {
+		List<String> filter = new ArrayList<String>();
+		filter.add("test");
+
+		loader.setFilter(filter);
+		index.setLoader(loader);
+
+		assertTrue(index.getInvertedIndex().get("ipsum") != null);
+	}
+
+	@Test
+	void testFilterMultiple() {
+		List<String> filter = new ArrayList<String>();
+		filter.add("txt");
+		filter.add("test");
+
+		loader.setFilter(filter);
+		index.setLoader(loader);
+		Set<String> test = index.getInvertedIndex().keySet();
+
+		assertTrue(test.contains("dog") && test.contains("ipsum"));
+	}
+
+	@Test
+	void testDepth() {
+		List<String> filter = new ArrayList<String>();
+		filter.add("txt");
+		filter.add("test");
+
+		loader.setFilter(filter);
+		loader.setDepth(1);
+		index.setLoader(loader);
+
+		assertNull(index.getInvertedIndex().get("ipsum"));
+	}
+
+	@Test
+	void testReturnType() {
+		assertEquals(HashMap.class, loader.getDocumentStringMap().getClass());
+	}
+
+	@Test
+	void testClassType() {
+		assertTrue(loader instanceof DocumentLoader);
+	}
+
+	@Test
+	void testNoResult() {
+		assertNull(index.getInvertedIndex().get("test"));
+	}
+
+	@Test
+	void testTermCount() {
+		assertEquals(index.getInvertedIndex().size(), 12);
+	}
+
+	@Test
+	void testInvalidPath() {
+		loader.setPath("%");
+		assertTrue(loader.getDocumentStringMap().isEmpty());
+	}
+
+	@Test
+	void testGetDepth() {
+		loader = new FileLoader("", 5);
+		assertEquals(5, loader.getDepth());
+	}
+
+	@Test
+	void testGetPath() {
+		loader.setPath("test");
+		assertEquals("test", loader.getPath().toString());
+	}
+
+	@Test
+	void testGetFilter() {
+		List<String> testFilter = new ArrayList<String>();
+		testFilter.add("test");
+
+		loader = new FileLoader("", testFilter);
+
+		assertEquals(testFilter, loader.getFilter());
+	}
+	
+	@Test
+	void testDepthAndFilterConstructor() {
+		List<String> testFilter = new ArrayList<String>();
+		testFilter.add("test");
+
+		loader = new FileLoader("", testFilter, 5);
+
+		assertTrue(loader.getFilter().equals(testFilter) && loader.getDepth() == 5);
+	}
 }
--- SimpleSearchEngine/src/test/java/se/anderssonbilly/simplesearchengine/testStringLoader.java
+++ SimpleSearchEngine/src/test/java/se/anderssonbilly/simplesearchengine/testStringLoader.java
@@ -1,13 +1,21 @@
 package se.anderssonbilly.simplesearchengine;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import se.anderssonbilly.simplesearchengine.documentloader.DocumentLoader;
 import se.anderssonbilly.simplesearchengine.documentloader.StringListLoader;
 import se.anderssonbilly.simplesearchengine.invertedindex.InvertedIndex;
 import se.anderssonbilly.simplesearchengine.tfidf.TfIdf;
@@ -39,4 +47,56 @@ class testStringLoader {
 		index = new InvertedIndex(loader);
 	}
 
+	@Test
+	void testReturnValue() {
+		expected.put("doc_1", new TfIdf(0,0));
+		expected.put("doc_3", new TfIdf(0,0));
+		
+		Set<String> test = index.getInvertedIndex().get("fox").keySet();
+		
+		assertTrue(test.containsAll(expected.keySet()) && !test.contains("doc_2"));
+	}
+	
+	@Test
+	void testSetStringList() {
+		List<String> testNewList = new ArrayList<>();
+		testNewList.add("Lorem ipsum dolor ipsum sit ipsum");
+		testNewList.add("Vituperata incorrupte at ipsum pro quo");
+		testNewList.add("Has persius disputationi id simul");
+		expected.put("doc_1", new TfIdf(0,0));
+		expected.put("doc_2", new TfIdf(0,0));
+		
+		loader.setStringList(testNewList);
+		index.setLoader(loader);
+		Set<String> test = index.getInvertedIndex().get("ipsum").keySet();
+		
+		assertTrue(test.containsAll(expected.keySet()) && !test.contains("doc_3"));
+	}
+	
+	@Test
+	void testReturnType() {
+		assertEquals(HashMap.class, loader.getDocumentStringMap().getClass());
+	}
+
+	@Test
+	void testClassType() {
+		assertTrue(loader instanceof DocumentLoader);
+	}
+	
+	@Test
+	void testNoResult() {
+		assertNull(index.getInvertedIndex().get("test"));
+	}
+	
+	@Test
+	void testTermCount() {
+		assertEquals(index.getInvertedIndex().size(),12);
+	}
+	
+	@Test
+	void testEmptyConstructor() {
+		loader = new StringListLoader();
+		
+		assertNotNull(loader.getDocumentStringMap());
+	}
 }
