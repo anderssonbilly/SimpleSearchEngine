0709e99112c7ff9d406fc6e2b1567605e52a6bb7
Billy Andersson <anderssonbilly@hotmail.com>
2019-03-26 18:27:47 +0100

    Fixed type and added example code in main

--- SimpleSearchEngine/src/main/java/se/anderssonbilly/simplesearchengine/Main.java
+++ SimpleSearchEngine/src/main/java/se/anderssonbilly/simplesearchengine/Main.java
@@ -1,10 +1,26 @@
 package se.anderssonbilly.simplesearchengine;
 
+import java.util.Map;
+
+import se.anderssonbilly.simplesearchengine.documentloader.FileLoader;
+import se.anderssonbilly.simplesearchengine.invertedindex.InvertedIndex;
+import se.anderssonbilly.simplesearchengine.tfidf.TfIdf;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Map<String,Map<String,TfIdf>> index = new InvertedIndex(new FileLoader("src/test/resources")).getInvertedIndex();
+		
+		index.forEach((term,docs) -> {
+			System.out.println("---");
+			System.out.println("Term: \"" + term + "\" occurs in:");
+			docs.forEach((docName,tfidf)->{
+				System.out.println(" \"" + docName + "\"");
+				System.out.println("  tf   : " + tfidf.getTf());
+				System.out.println("  idf  : " + tfidf.getIdf());
+				System.out.println("  tfidf: " + tfidf.getTfIdf());
+			});
+		});
 	}
 
 }
